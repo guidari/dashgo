a8c0d57fd1ecd69cdb3057cb8f1ca447b62fc2fd
Guilherme Datilio Ribeiro1 <guilherme.datilio@ibm.com>
2022-07-01 23:25:27 -0300

    Hotfix sidebar items

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -17,13 +17,13 @@ export function SideBar() {
           <Stack spacing="4" mt="8" align="stretch">
             <Link display="flex" alignItems="center">
               <Icon as={RiDashboardLine} fontSize="20"></Icon>
-              <Text mt="4" fontWeight="medium">
+              <Text fontWeight="medium" ml="2">
                 Dashboard
               </Text>
             </Link>
             <Link display="flex" alignItems="center">
               <Icon as={RiContactsLine} fontSize="20"></Icon>
-              <Text mt="4" fontWeight="medium">
+              <Text fontWeight="medium" ml="2">
                 Usuários
               </Text>
             </Link>
@@ -37,13 +37,13 @@ export function SideBar() {
           <Stack spacing="4" mt="8" align="stretch">
             <Link display="flex" alignItems="center">
               <Icon as={RiInputMethodLine} fontSize="20"></Icon>
-              <Text mt="4" fontWeight="medium">
+              <Text fontWeight="medium" ml="2">
                 Dashboard
               </Text>
             </Link>
             <Link display="flex" alignItems="center">
               <Icon as={RiGitMergeLine} fontSize="20"></Icon>
-              <Text mt="4" fontWeight="medium">
+              <Text fontWeight="medium" ml="2">
                 Automação
               </Text>
             </Link>
