aeecd47543706479dc93a8da45094dea250aba3e
Guilherme Datilio Ribeiro1 <guilherme.datilio@ibm.com>
2022-07-01 21:33:52 -0300

    Sidebar structure created

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,55 @@
+import { Box, Icon, Link, Stack, Text } from "@chakra-ui/react";
+import {
+  RiDashboardLine,
+  RiContactsLine,
+  RiGitMergeLine,
+  RiInputMethodLine,
+} from "react-icons/ri";
+
+export function SideBar() {
+  return (
+    <Box as="aside" w="64" mr="8">
+      <Stack spacing="12" align="flex-start">
+        <Box>
+          <Text fontWeight="bold" color="gray.400" fontSize="small">
+            Geral
+          </Text>
+          <Stack spacing="4" mt="8" align="stretch">
+            <Link display="flex" alignItems="center">
+              <Icon as={RiDashboardLine} fontSize="20"></Icon>
+              <Text mt="4" fontWeight="medium">
+                Dashboard
+              </Text>
+            </Link>
+            <Link display="flex" alignItems="center">
+              <Icon as={RiContactsLine} fontSize="20"></Icon>
+              <Text mt="4" fontWeight="medium">
+                Usuários
+              </Text>
+            </Link>
+          </Stack>
+        </Box>
+
+        <Box>
+          <Text fontWeight="bold" color="gray.400" fontSize="small">
+            AUTAMAÇÃO
+          </Text>
+          <Stack spacing="4" mt="8" align="stretch">
+            <Link display="flex" alignItems="center">
+              <Icon as={RiInputMethodLine} fontSize="20"></Icon>
+              <Text mt="4" fontWeight="medium">
+                Dashboard
+              </Text>
+            </Link>
+            <Link display="flex" alignItems="center">
+              <Icon as={RiGitMergeLine} fontSize="20"></Icon>
+              <Text mt="4" fontWeight="medium">
+                Automação
+              </Text>
+            </Link>
+          </Stack>
+        </Box>
+      </Stack>
+    </Box>
+  );
+}
